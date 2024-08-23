c6c705ce47491cfc1ab91e54e4d15d58ad6dbf4e
anutkaavslchnk <anutochkaaaaa14@gmail.com>
2024-08-23 22:18:38 +0200

    Update contactsSlice.js

--- src/redux/contacts/contactsSlice.js
+++ src/redux/contacts/contactsSlice.js
@@ -18,34 +18,28 @@ const initialState = {
 const contactsSlice = createSlice({
   name: 'contacts',
   initialState,
-
-  extraReducers:builder=>{
-    builder.addCase(fetchContacts.fulfilled, (state, action)=>{
-      state.items=action.payload;
-      state.isLoading=false;
-    })
-    .addCase(fetchContacts.pending, (state, action)=>{
-      state.isLoading=true;
-
-    })
-  
-    .addCase(deleteContact.fulfilled,(state, action)=>{
-      state.items=state.items.filter(item=> item.id !== action.payload);
-      toast.success('Contact is deleted!');
-    })
-    .addCase(addContact.fulfilled, (state, action)=>{
-      state.items.push(action.payload);
-       toast.success('Contact is added!');
-    
-     
-    })
-    .addCase(deleteContact.rejected,(state, action)=>{
-     state.isError=true;
-     toast.error('Sorry, something went wrong, try again!');
-    })
-  
+  extraReducers: builder => {
+    builder
+      .addCase(fetchContacts.fulfilled, (state, action) => {
+        state.items = action.payload;
+        state.isLoading = false;
+      })
+      .addCase(fetchContacts.pending, state => {
+        state.isLoading = true;
+      })
+      .addCase(deleteContact.fulfilled, (state, action) => {
+        state.items = state.items.filter(item => item.id !== action.payload);
+        toast.success('Contact is deleted!');
+      })
+      .addCase(addContact.fulfilled, (state, action) => {
+        state.items.push(action.payload);
+        toast.success('Contact is added!');
+      })
+      .addCase(deleteContact.rejected, state => {
+        state.isError = true;
+        toast.error('Sorry, something went wrong, try again!');
+      });
   }
 });
-
 
 export const contactsSliceReducer = contactsSlice.reducer;
