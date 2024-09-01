04aadcb31a2b49187a44507883c687f7d768dab0
anutkaavslchnk <anutochkaaaaa14@gmail.com>
2024-09-01 18:24:56 +0200

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,7 +5,7 @@ const HomePage = () => {
     <>
     <h2 className={s.greetings}>Welcome to the the contact book app! <br/>We hope that this app will be useful for you!
     </h2>
-    <img  className={s.photo}src='/src/img/phone.jpg' alt='phone'></img>
+    <img src='/src/img/phone.jpg' alt='phone'  className={s.photo} />
     </>
   )
 };
